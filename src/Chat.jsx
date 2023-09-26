c3167402e4b6bde647266f991c5687a5a63ac38e
abhigyanbasak248 <96128313+abhigyanbasak248@users.noreply.github.com>
2023-09-26 19:44:35 +0530

    Add files via upload

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,94 +1,91 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 const Chat = () => {
   const [messages, setMessages] = useState([
-    { text: "message1", isUser: false },
-    { text: "message2", isUser: true },
-    { text: "message3", isUser: false },
+    { text: "Hello! How can I help you today?", isUser: false },
   ]);
-
   const [newMessage, setNewMessage] = useState("");
-  // const [isPanelOpen, setIsPanelOpen] = useState(false);
+
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   const handleSendMessage = () => {
     if (newMessage.trim() !== "") {
-      setMessages([...messages, { text: newMessage, isUser: true }]);
+      const userMessage = { text: newMessage, isUser: true };
+      setMessages((prevMessages) => [...prevMessages, userMessage]);
       setNewMessage("");
 
       axios
-        .post("http://localhost:5000/chatbot/" + encodeURIComponent(newMessage))
+        .post("http://127.0.0.1:5000/chatbot/" + encodeURIComponent(newMessage))
         .then((response) => {
-          setMessages([
-            ...messages,
-            { text: response.data.Answer, isUser: false },
-          ]);
+          const botMessage = { text: response.data.Answer, isUser: false };
+          setMessages((prevMessages) => [...prevMessages, botMessage]);
+          scrollToBottom(); // Scroll to the latest message after it's added
         })
         .catch((error) => console.error("Error:", error));
     }
   };
 
-  // const togglePanel = () => {
-  //   setIsPanelOpen(!isPanelOpen);
-  // };
-
-  // const openPanel = () => {
-  //   setIsPanelOpen(true);
-  // };
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSendMessage();
+    }
+  };
 
   return (
     <>
-      <div className="bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100">
-        <div className="flex h-screen w-3/4 m-auto ">
-          {/* <div className={`w-64 bg-white p-4 transition-all ease-in-out duration-300 ${isPanelOpen ? 'translate-x-0' : '-translate-x-64'}`}>
-          
-          <button onClick={togglePanel}>Toggle Panel</button>
-        </div> */}
-          <div className="flex-grow w-1/2 p-4 overflow-y-auto">
-            {messages.map((message, index) => (
+      <div className="bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 h-screen flex flex-col">
+        <div className="flex-grow p-4 overflow-y-auto">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`mb-4 flex ${
+                message.isUser ? "justify-end" : "justify-start"
+              }`}
+            >
               <div
-                key={index}
-                className={`mb-4 flex ${
-                  message.isUser ? "justify-end" : "justify-start"
-                }`}
+                className={`${
+                  message.isUser
+                    ? "bg-indigo-700 text-white"
+                    : "bg-gray-200 text-gray-700"
+                } p-2 rounded-lg max-w-md`}
               >
-                <div
-                  className={`${
-                    message.isUser
-                      ? "bg-indigo-700 text-white"
-                      : "bg-gray-200 text-gray-700"
-                  } p-2 rounded-lg max-w-md`}
-                >
-                  {message.text}
-                </div>
+                {message.text}
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
+          <div ref={messagesEndRef} />
         </div>
-      </div>
-      {/* <div className="fixed bottom-4 left-4 bg-indigo-500 text-white px-4 py-2 rounded-full">
-        <button onClick={openPanel}>Open Panel</button>
-      </div> */}
-      <div className="fixed bottom-0 left-0 right-0 p-12">
-        <div className="flex items-center">
-          <input
-            type="text"
-            className="flex-grow px-2 py-1 border rounded-l-lg focus:outline-none"
-            placeholder="Type a message..."
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-          />
-          <button
-            className={`${
-              newMessage.trim() !== ""
-                ? "bg-indigo-500 text-white"
-                : "bg-gray-400 text-gray-700"
-            } px-4 py-2 rounded-r-lg`}
-            onClick={handleSendMessage}
-            disabled={newMessage.trim() === ""}
-          >
-            Send
-          </button>
+        <div className="p-12">
+          <div className="flex items-center">
+            <input
+              type="text"
+              className="flex-grow px-2 py-1 border rounded-l-lg focus:outline-none"
+              placeholder="Type a message..."
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              onKeyPress={handleKeyPress}
+            />
+            <button
+              className={`${
+                newMessage.trim() !== ""
+                  ? "bg-indigo-500 text-white"
+                  : "bg-gray-400 text-gray-700"
+              } px-4 py-2 rounded-r-lg`}
+              onClick={handleSendMessage}
+              disabled={newMessage.trim() === ""}
+            >
+              Send
+            </button>
+          </div>
         </div>
       </div>
     </>
